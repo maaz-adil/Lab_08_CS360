114121c9d640042416973ae215ff9c633cf7554d
maaz-adil <maazadil2004@gmail.com>
2026-03-18 23:48:55 +0500

    failing test: hasCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,11 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
-
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList(null);
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+    }
 }
